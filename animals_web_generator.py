daff9937423d68fa05ceb4dc23b8ba453d2db203
koray92 <112970299+koray92@users.noreply.github.com>
2025-01-31 17:25:12 +0100

    bring animal data into html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -8,21 +8,34 @@ def load_data(file_path):
 animals_data = load_data("animals_data.json")
 
 def get_animal_name():
+    output = []
     for i in animals_data:
         skip_type = i.get("characteristics", {}).get("type", "Unknown")
         if skip_type == "Unknown":
-            print(f"Name: {i["name"]}\n"
-                  f"Diet: {i["characteristics"]["diet"]}\n"
-                  f"Location: {i["locations"][0]}")
+            output.append(f"Name: {i["name"]}\n"
+                          f"Diet: {i["characteristics"]["diet"]}\n"
+                          f"Location: {i["locations"][0]}")
+
         else:
-            print(f"Name: {i["name"]}\n"
-                  f"Diet: {i["characteristics"]["diet"]}\n"
-                  f"Location: {i["locations"][0]}\n"
-                  f"Type: {i["characteristics"]["type"]}")
+            output.append(f"Name: {i["name"]}\n"
+                          f"Diet: {i["characteristics"]["diet"]}\n"
+                          f"Location: {i["locations"][0]}\n"
+                          f"Type: {i["characteristics"]["type"]}")
+
+    result = "\n\n".join(output)
+    return result
 
 
+def read_html(file_path):
+    with open(file_path, "r") as f:
+        file_str = f.read()
+        return file_str
 
 
+html_file = str(read_html("animals_template.html"))
+animals_str = str(get_animal_name())
 
+new_html = html_file.replace("__REPLACE_ANIMALS_INFO__", animals_str)
 
-get_animal_name()
+with open("animals.html", "w") as f:
+    f.write(new_html)
